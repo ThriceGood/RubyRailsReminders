20c6239dd53a81ca69ba154d8380e9166371cff2
ThriceGood <jonathan.norris88@hotmail.com>
2024-02-11 16:32:41 +0100

    readability improvement

--- lib/reminders/manager.rb
+++ lib/reminders/manager.rb
@@ -13,21 +13,39 @@ module Reminders
     def self.create_reminders_for(ticket)
       reminder_jobs = []
       assignee = ticket.assignee
-      send_date = (ticket.due_date - assignee.due_date_reminder_day_offset.days).to_date
+      send_times = self.reminder_send_times(ticket)
 
-      while send_date <= ticket.due_date
-        Time.use_zone(assignee.time_zone) do 
-          send_time = Time.zone.parse("#{send_date} #{assignee.due_date_reminder_time}")
-          next if send_time < Time.current
-
-          assignee.configured_reminder_types.each do |reminder_type|
-            reminder_jobs << self.reminder_of_type(reminder_type).schedule(ticket, send_time)
-          end
+      send_times.each do |send_time|
+        assignee.configured_reminder_types.each do |reminder_type|
+          reminder_jobs << self.reminder_of_type(reminder_type).schedule(ticket, send_time)
         end
-        send_date += assignee.due_date_reminder_interval.days
       end
 
       reminder_jobs
+    end
+
+    private
+
+    def self.reminder_send_times(ticket)
+      send_times = []
+      assignee = ticket.assignee
+      day_offset = assignee.due_date_reminder_day_offset.days
+      reminder_time = assignee.due_date_reminder_time
+      reminder_interval = assignee.due_date_reminder_interval.days
+
+      send_date = (ticket.due_date - day_offset).to_date
+
+      while send_date <= ticket.due_date
+        Time.use_zone(assignee.time_zone) do 
+          send_time = Time.zone.parse("#{send_date} #{reminder_time}")
+          next if send_time < Time.current
+
+          send_times << send_time
+        end
+        send_date += reminder_interval
+      end
+
+      send_times
     end
 
     def self.reminder_of_type(reminder_type)
@@ -36,6 +54,7 @@ module Reminders
       "Reminders::#{reminder_type.capitalize}".constantize
     end
 
+    private_class_method :reminder_send_times
     private_class_method :reminder_of_type
   end
 end
